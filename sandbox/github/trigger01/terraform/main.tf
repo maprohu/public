6b12003bd6c31a47c98b0011a61dc2e6c90fdaca
maprohu <maprohu@gmail.com>
2023-11-22 19:43:08 +0000

    aws

--- sandbox/github/trigger01/terraform/main.tf
+++ sandbox/github/trigger01/terraform/main.tf
@@ -57,12 +57,12 @@ resource "aws_lambda_function" "trigger_github_action" {
 
   environment {
     variables = {
-      GITHUB_APP_ID = var.github_app_id
-      GITHUB_APP_PRIVATE_KEY = var.github_app_private_key
+      GITHUB_APP_ID              = var.github_app_id
+      GITHUB_APP_PRIVATE_KEY     = var.github_app_private_key
       GITHUB_APP_INSTALLATION_ID = var.github_app_installation_id
-      GITHUB_OWNER = var.github_owner
-      GITHUB_REPO = var.github_repo
-      GITHUB_EVENT_TYPE = var.github_event_type
+      GITHUB_OWNER               = var.github_owner
+      GITHUB_REPO                = var.github_repo
+      GITHUB_EVENT_TYPE          = var.github_event_type
     }
   }
 }
@@ -74,4 +74,102 @@ resource "aws_lambda_function_url" "lambda" {
 
 output "function_url" {
   value = aws_lambda_function_url.lambda.function_url
+}
+
+resource "null_resource" "yarn_install" {
+  provisioner "local-exec" {
+    command = "yarn --cwd ${path.module}/../ui install"
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+resource "null_resource" "yarn_build" {
+  depends_on = [null_resource.yarn_install]
+  provisioner "local-exec" {
+    command = "yarn --cwd ${path.module}/../../ui build"
+    environment = {
+      "PUBLIC_API_URL" = aws_lambda_function_url.lambda.function_url
+    }
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+data "archive_file" "ui" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../ui/dist"
+  output_path = "${path.module}/../build/ui.zip"
+}
+resource "aws_s3_bucket" "ui" {
+  bucket        = "github-trigger-ui"
+  force_destroy = true
+}
+resource "aws_s3_bucket_website_configuration" "ui" {
+  bucket = aws_s3_bucket.ui.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+output "ui_url" {
+  value = "http://${aws_s3_bucket_website_configuration.ui.website_endpoint}"
+}
+resource "aws_s3_bucket_ownership_controls" "ui" {
+  bucket = aws_s3_bucket.ui.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "ui" {
+  bucket = aws_s3_bucket.ui.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "ui" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ui,
+    aws_s3_bucket_public_access_block.ui,
+  ]
+
+  bucket = aws_s3_bucket.ui.id
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "ui" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.ui.arn}/*",
+    ]
+    actions = ["S3:GetObject"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.ui.id
+  policy = data.aws_iam_policy_document.ui.json
+}
+
+resource "null_resource" "s3_sync" {
+  depends_on = [aws_s3_bucket.ui, null_resource.yarn_build]
+  provisioner "local-exec" {
+    command = "aws s3 sync ${path.module}/../../ui/dist s3://${aws_s3_bucket.ui.bucket} --delete"
+    environment = {
+      "PUBLIC_API_URL" = aws_lambda_function_url.lambda.function_url
+    }
+  }
+  triggers = {
+    always_run = data.archive_file.lambda.output_sha256
+  }
+
 }
---
+++ sandbox/github/ui/src/routes/index.tsx
@@ -0,0 +1,44 @@
+import type { JSXNode} from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  const apiUrl = import.meta.env.PUBLIC_API_URL as string;
+  const started = useSignal(false);
+
+  function content(): JSXNode {
+    if (started.value) {
+      return <>
+        Deploy Started
+        <br/>
+        <button onClick$={() => {
+          started.value = false;
+        }}>Back</button>
+      </>
+    } else {
+      return <>
+        <button onClick$={() => {
+          fetch(apiUrl);
+          started.value = true;
+        }}>Start Deploy</button>
+      </>
+    }
+  }
+
+
+  return (
+    <>
+      {content()}
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+}; 
